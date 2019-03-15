3a16e64cda51a03d50429db573affec00b44ecda
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-03-15 23:53:01 +0800

    New Docs to routes of Auth Service:     Added alot more documentation to both the tokens and user route handler modules     in the routes directory, to finally layout how the code will work / flow. It     was slightly confusing at first on how everything will be put together, but now     alot more descriptive info has been added for how everything will work together

--- server/Auth_service/routes/tokens.js
+++ server/Auth_service/routes/tokens.js
@@ -1,19 +1,47 @@
 'use strict'; // Enforce use of strict verion of JavaScript
 
 /*	@Doc
-	API routes for dealing with tokens
+    API routes for dealing with tokens
+    
+    1.  User posts credential to server on the '/login' route
+    2.  Login route's handler will authenticate user's credential against those in the Database
+    3.  If the credentials are valid,
+            - Generate a JWT/Refresh-token pair
+            - Put the tokens into the response headers
+            - Put the redirect location towards the user homepage into the response headers
+            - Respond back to the client with the Response messages
+
+
+    'tokens' and 'login' routes are both used for exchanging Credentials for JWTs
+    When user is on the login page, the creds. should be posted to the /login route
+    When user is not on the login page but wants to post creds. for JWTs, then the /token route should be used instead
 */
 
-// Route to get token from the service
-app.post('/tokens', express.json(), (req, res) => {
-    /*  @Code-flow
-        Use the bodyParser middleware to read the user credentials from the request body
-        Maybe Clean/Sanitize the credentials before using it against the DB?
-        Get the password hash of user with "userID" from the DataBase
-        compare it with the password user posted
-        Create a JWT and send it back to the user
-    */
-});
+// Middleware function for authenticating Credentials against those stored in the Database
+function authenticate (req, res, next) {
+
+}
+
+// Middleware function for creating JWT payload for the client, creating and signing the JWT and finally attaching it for the user to use
+function authenticate (req, res, next) {
+
+}
+
+/*  Routes to get token from the service
+    2 Additional middlewares, one for authenticating the Creds, and one for creating and attaching token
+    
+    @Code-flow
+    Use the bodyParser middleware to read the user credentials from the request body
+    Maybe Clean/Sanitize the credentials before using it against the DB?
+    Get the password hash of user with "userID" from the DataBase
+    compare it with the password user posted
+    Create a JWT and send it back to the user
+*/
+app.post('/login', express.json(), authenticate, attach_token);
+app.post('/tokens', express.json(), authenticate, attach_token);
+// See if the below format is possible
+app.post('/tokens or login', express.json(), authenticate, attach_token);
+
 
 // Route to get a new JWT with a complimentary refresh token
 app.post('/tokens/refresh', express.json(), (req, res) => {
--- server/Auth_service/routes/user.js
+++ server/Auth_service/routes/user.js
@@ -1,7 +1,13 @@
 'use strict'; // Enforce use of strict verion of JavaScript
 
 /*	@Doc
-	API routes for users
+    API routes for users
+    
+    @Todo
+    - Modify the user route to be JWT protected
+    - Create a new /user route without the userID, which basically just
+      accepts a req from the client, read its JWT to check for the userID
+      and then redirects the client to the route that holds their userID
 */
 
 // Route to get the user object back from the DB
