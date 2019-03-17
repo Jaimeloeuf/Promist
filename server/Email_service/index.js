a0b042a62f781c7ec32508f9fab3fab11e55132b
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-03-17 22:01:57 +0800

    Email Service server app module update: 1.  Rewrote the middleware for checking the identity and role of the JWT bearer.     Currently there is 2 versions, 1 using the Promise API and the other, the     async/await.
    2.  For the '/send' route, finally implemented a proper middleware execution
        sequence, to properly process in the incoming requests in a modularized
        fashion, instead of doing it all in the anonymous middleware function.
    
    3.  Added proper documentation for the '/send' route's middleware sequencing
        and also how each part works and why.
    
    4.  Removed the v2 route handler as the functionalities are being refactored
        out into middlewares.

--- server/Email_service/index.js
+++ server/Email_service/index.js
@@ -2,9 +2,9 @@
 
 /*	@Doc
 	Server app instance module that handles incoming email sending requests from other services
-	
-	This service will be upgraded to use KoaJS
-	If koa JS used, body-parser package may be needed
+    
+    @Todo
+	Update service to use KoaJS, note that body-parser package need be independently installed
 */
 
 
@@ -17,45 +17,71 @@ const { print, error, JSON_string } = require('./utils');
 
 
 /*  @Doc
-    This is a middleware function that will use the token module to read out the token and place
-    it into the req.token property and pass it to the next middleware. Which will verify if the
-    client with the JWT should be allowed to continue with the route
-    Only client with JWTs that specify admin privileges can access the route handler.
+    verify if the client with the JWT should be allowed to continue with the route
+    Only client with JWTs that specify service roles can access the route handler.
 
-    !!! THe token middleware should be ran before the express.json() parser!!! So only authed ppl can
-    Send the HTTP body over to the server
+
+    Public private key
+    The services themselves hold the key and methods for verifying JWTs but not signing JWTs
+    Only the Auth Service or Token Service can sign JWTs.
+    How to use JWTs to verify identiy of the services?
+    If using symmetric key signing then it is easy as the keys are the same
+    But with Asymmetric, then the problem arises
 */
-function token(req, res, next) {
-    next();
-}
-
-// @Todo Add a POST body size limit to the bodyParser
-// Email requests' route, protected with JWT identity verification
-// Allows optional requests for the email service to respond back when the service is performed
-app.post('/send', express.json(), token, (req, res) => {
-    // Request body is the JSON string parsed by express.json() for send_mail function
-    // The bodyParser middleware will only be used for the '/send' route
-    send_mail(req.body);
-    // End the HTTP req/res cycle after call to send_mail function
-    res.end();
-});
-
-// /Send route v2 with JWT based route protection
-app.post('/send', express.json(), (req, res) => {
-    // Example usage
-    verify(token)
+function service_identity_checker(req, res, next) {
+    // This middleware uses the Promise API
+    verify(req.token)
         .then((token) => {
-            // Check if the token is valid for the particular route
-            // Check for example if the user is an admin
-            if (token.role === 'admin')
-                send_mail(req.body);
+            // If token is valid, Check if the role is a service
+            if (token.role === 'service')
+                next(); // Call next middleware to execute
             else
                 throw new Error('ERR: Forbidden route');
         })
         .catch((err) => {
             // Log the error to the error log for analytics
-        })
-        .finally(() => res.end()); // End the req/response cycle
+
+            // End this req/res cycle
+        });
+}
+
+async function service_identity_checker(req, res, next) {
+    // This middleware uses the async/await API
+    try {
+        const token = await verify(req.token);
+        if (token.role === 'service')
+            next(); // Call next middleware to execute
+
+        // Or maybe the below check also can depend on how the JWT is structured
+        // Check if the service field is true
+        if (req.token.service)
+            next();
+    } catch (err) {
+        // Log the error to the error log for analytics
+
+        // End this req/res cycle
+    }
+}
+
+/*  @Flow
+    - Extract token out from request header and attach directly to request object's token property
+    - Verify that the JWT signature is valid
+    - Check the identity of the client as stated in the JWT to make sure client is an approved service
+    - Accept and read the HTTP message body posted from the client
+    - Send the mail as specified by the request body (Only this is done in the anoynymous function)
+
+    @Todos
+    - How to verify services with tokens? Who will sign the token? Will it be a central authority solution or a decentralized version
+    - If token is expired, should I redirect user to go and signin again or refresh token automatically
+    - Add a POST body size limit to the bodyParser
+    - Take in an optional request on the url for email service to respond back when the service is performed
+*/
+app.post('/send', get_token, service_identity_checker, express.json(), (req, res) => {
+    // Request body is the JSON string parsed by express.json() for send_mail function
+    // The bodyParser middleware will only be used for the '/send' route
+    send_mail(req.body);
+    // End the HTTP req/res cycle after call to send_mail function
+    res.end();
 });
 
 
