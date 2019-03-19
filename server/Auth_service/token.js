133f5b1b83c100e970fe05dbf69e64bb266b2801
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-03-19 15:40:59 +0800

    Auth Service token module cleanup: 1.  Removed all the unneeded dependencies and added in the dependencies from     the self made jwt module.
    2.  Cleaned up and removed alot of functions that are either deprecated or
        refactored out into the new jwt module.
    
    3.  Added signing and verifying options object to use for the creation and
        verification of tokens with the functions from the jwt module.
    
    4.  Removed the whole bunch of documentation at the bottom which is now
        refactored into the jwt module.

--- server/Auth_service/token.js
+++ server/Auth_service/token.js
@@ -14,33 +14,52 @@
 */
 
 // Dependencies
-const jwt = require('jsonwebtoken'); // External dependency from NPM by Auth0
-const { promisify } = require('util');
-
-// Using the promisify method from the util module, Promisify the original jwt methods
-const jwtSignAsync = promisify(jwt.sign);
-const jwtVerifyAsync = promisify(jwt.verify);
+const { extract_token, create_token, verify } = require('./jwt');
 
 // Variables used for signing/verifying tokens. Should be read from env or config file.
 const expiresAfter = '100s';
-const signageKey = 'secret';
 
-/*  Promisified version of jwt.sign method with Signing key and options in its closure.
-    Resolves with the signed JWT, else
-    Rejects with an error.  */
-const create_token = (payload) => jwtSignAsync(payload, signageKey, { expiresIn: expiresAfter });
+/*
+    Payload given to function should only contain private claims and should not hold any
+    of the pre-registered interoperable claim names and values.
 
-/*  Promisified version of jwt.verify method with Signing key in its closure.
-    If signature is valid and the optional expiration, audience, or issuer are valid if given
-    Resolves with the decoded token, else
-    Rejects with an error.  */
-const verify = (token) => jwtVerifyAsync(token, signageKey);
+    User to specify the sign and verify options directly in this module for every service
+    The OPTIONS MUST BE DEFINED, and they must be defined inside this module,
+    thus for the jwt module, the signOptions can be the parameter for the first function
+*/
 
+
+// SIGNING OPTIONS
+var signOptions = {
+    issuer: 'Mysoft corp',
+    subject: 'some@user.com',
+    // audience: 'https://Promist.io',
+    audience: ['https://Promist.io', '.... all the services names'],
+    expiresIn: "12h",
+    algorithm: "RS256"
+};
+
+/* The sign and verify options for the token module of the Auth Service */
+const signOption = { expiresIn: expiresAfter };
+const verifyOption = {};
+
+
+var verifyOptions = {
+    issuer: 'Mysoft corp',
+    subject: 'some@user.com',
+    // audience: 'https://Promist.io',
+    audience: ['https://Promist.io', '.... all the services names'],
+    algorithm: ["RS256"] // Unlike signOption that we used while signing new token , we will use verifyOptions to verify the shared token by client. The only difference is, here the algorithm is Array [“RS256”].
+};
+
+// Modify the create_token function by adding signOption object into new function's closure
+create_token = create_token(signOption);
+// Modify the create_token function by adding signOption object into new function's closure
+verify = verify(verifyOption);
 
 /*  Token verification middleware:
     To be passed in to the routes before the route handlers.
-    If the token is invalid, it will secure the route by automatically ending the req/res cycle
-*/
+    If the token is invalid, it will secure the route by automatically ending the req/res cycle */
 function v_mw(req, res, next) {
     // Middleware to call the verify function first to make sure JWT is valid
 
@@ -53,14 +72,6 @@ function v_mw(req, res, next) {
     next();
 }
 
-
-/*  Pure function to extract token from request header and returns it
-    FORMAT OF TOKEN
-    Authorization: Bearer <access_token>
-
-    Split on space and Get token from array and return the token
-*/
-const extract_token = (req) => req.headers['authorization'].split(' ')[1];
 
 // This is a middleware function that can be used on routes that require JWT security
 function get_token(req, res, next) {
@@ -80,51 +91,7 @@ module.exports = {
     // The middleware for extracting token into request object's token property
     get_token,
 
-    // The 2 promisified methods from the 'jwt' module
-    jwtSignAsync,
-    jwtVerifyAsync,
-
     // The 2 modified versions for token signing and verification with the key in their closures
     create_token,
     verify
 }
-
-
-/*	Docs and Notes:
-
-    What should a JWT contain?   (Client holding the JWT will be referred to as the owner)
-	- The owner's Identity, basically declaring who the user is
-	- What are the resources that the owner can access.
-	- Who issused the JWT token to the user
-	- And who is the JWT intended for? Meaning who or which microservice should accept the token?
-	standard token
-	headers:
-		{
-			"typ": "JWT",
-			"alg": "HS256" // The algorithm used for the signature is HMAC SHA-256
-		}
-		{
-			// Who this person is (sub, short for subject)
-			// What this person can access with this token (scope)
-			// When the token expires (exp)
-			// Who issued the token (iss, short for issuer)
-			// These below declarations are known as Claims, because the token creator claims a set of assertions that can be used to ‘know’ things about the subject. Because the token is signed with a secret key, you can verify its signature and implicitly trust what is claimed.
-			"exp": ,
-			"iat": ,
-			"expiresIn": ,
-			"tokenType": "Bearer",
-			"sub":
-			"subject": "retrieve data", // What is the purpose of this token/request?
-			"usrID": 578ec9,
-			"usr": "john@gmail.com",
-			"iss": "bouk.com", // Issuer of the token
-			"aud": "bouk.com/", // Intended audience that should acccept the token
-			"account type": "consumer", // The type of account that the user has
-			"roles": {
-				// The things/roles that the user is allowed to do
-				"role": "consumer"
-				"booking": "create"
-			}
-			"scope": ["read", "write", "update", "del"]
-		}
-*/
