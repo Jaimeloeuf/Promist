c8824a15eec85e89a4814d0f2f144286a0ea28c8
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-03-20 09:22:50 +0800

    Auth Service token module cleanup     Cleaned up the token module as working on building this module to be an     abstraction module above the jwt module that includes the signing and     verification options "hardcoded" for this service in.

--- server/Auth_service/token.js
+++ server/Auth_service/token.js
@@ -5,7 +5,6 @@
 	This does not handle and authentication. Just token based Identity verification
 
     @TODO
-    - Add a function to coerce Auth header to either all lower or upper case
     - Finnish writing the verification middleware
 	- look into private Keys and stuff like Asymmetric signing and verifying
 	- Create child processes too, to deal with the parsing and signing as it seems like it
@@ -16,45 +15,34 @@
 // Dependencies
 const { extract_token, create_token, verify } = require('./jwt');
 
-// Variables used for signing/verifying tokens. Should be read from env or config file.
-const expiresAfter = '100s';
-
-/*
-    Payload given to function should only contain private claims and should not hold any
-    of the pre-registered interoperable claim names and values.
-
-    User to specify the sign and verify options directly in this module for every service
+/*  User to specify the sign and verify options directly in this module for every service
     The OPTIONS MUST BE DEFINED, and they must be defined inside this module,
-    thus for the jwt module, the signOptions can be the parameter for the first function
-*/
+    thus for the jwt module, the signOptions is the first function's parameter */
 
-
-// SIGNING OPTIONS
+// Token Signing OPTIONS
 var signOptions = {
     issuer: 'Mysoft corp',
     subject: 'some@user.com',
     // audience: 'https://Promist.io',
     audience: ['https://Promist.io', '.... all the services names'],
-    expiresIn: "12h",
-    algorithm: "RS256"
+    expiresIn: '100s',
+    algorithm: 'RS256'
 };
 
-/* The sign and verify options for the token module of the Auth Service */
-const signOption = { expiresIn: expiresAfter };
-const verifyOption = {};
-
-
+// Token verification  OPTIONS
 var verifyOptions = {
     issuer: 'Mysoft corp',
     subject: 'some@user.com',
     // audience: 'https://Promist.io',
     audience: ['https://Promist.io', '.... all the services names'],
-    algorithm: ["RS256"] // Unlike signOption that we used while signing new token , we will use verifyOptions to verify the shared token by client. The only difference is, here the algorithm is Array [“RS256”].
+    algorithm: ['RS256'] // Unlike signOption that we used while signing new token , we will use verifyOptions to verify the shared token by client. The only difference is, here the algorithm is Array [“RS256”].
 };
 
-// Modify the create_token function by adding signOption object into new function's closure
+// Return a function with signOption object in its closure and assign function back to create_token
+/* Payload given to function should only contain private claims and should not hold any
+of the pre-registered interoperable claim names and values. */
 create_token = create_token(signOption);
-// Modify the create_token function by adding signOption object into new function's closure
+// Return a function with verifyOption object in its closure and assign function back to verify
 verify = verify(verifyOption);
 
 /*  Token verification middleware:
