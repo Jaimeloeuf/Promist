d5f28df512990922ac61af844166f9eaabc04a48
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-03-21 15:56:36 +0800

    Auth Service token module: 1.  Updated the documentation for a clearer description of the module.
    2.  Corrected the dependency imports following a API change by the jwt module.
        The variable type has also been changed to var from const, as the variables are
        reassigned the partial application of the original functions they referenced
    
    3.  The Sign options have been edited to change the Expire in property and to
        add abit more docs on what is going on.
    
    4.  Function call in the get_token function is also changed following the API
        change made in the jwt module.
    
    5.  Export values are also updated following the API change, and the addition of
        a publicKey to be exported too.

--- server/Auth_service/token.js
+++ server/Auth_service/token.js
@@ -1,19 +1,20 @@
 'use strict'; // Enforce use of strict verion of JavaScript
 
 /*	@Doc Module description:
-	This library module will handle the create/read/sign/verify operations on a given token
-	This does not handle and authentication. Just token based Identity verification
+    This module wraps over the jwt module to apply sign and verify options into the methods
+    from the jwt module before exporting the functions returned by these partial applications.
+    Essentially the sign and verify options specific to this service is defined in here.
 
     @TODO
     - Finnish writing the verification middleware
 	- look into private Keys and stuff like Asymmetric signing and verifying
 	- Create child processes too, to deal with the parsing and signing as it seems like it
       will take quite abit of CPU power
-    - Start implementing JWEs
+    - Start implementing JWEs (Is JWEs needed if there are stored in a HttpOnly cookie?)
 */
 
 // Dependencies
-const { extract_token, create_token, verify } = require('./jwt');
+var { extract_jwt_in_header, create_token, verify_token, publicKey } = require('./jwt');
 
 /*  User to specify the sign and verify options directly in this module for every service
     The OPTIONS MUST BE DEFINED, and they must be defined inside this module,
@@ -25,8 +26,8 @@ var signOptions = {
     subject: 'some@user.com',
     // audience: 'https://Promist.io',
     audience: ['https://Promist.io', '.... all the services names'],
-    expiresIn: '100s',
-    algorithm: 'RS256'
+    expiresIn: '10m', // Give the token a 10min lifetime
+    algorithm: 'RS256' // Must be RS256 as using asymmetric signing
 };
 
 // Token verification  OPTIONS
@@ -38,12 +39,13 @@ var verifyOptions = {
     algorithm: ['RS256'] // Unlike signOption that we used while signing new token , we will use verifyOptions to verify the shared token by client. The only difference is, here the algorithm is Array [“RS256”].
 };
 
+/*  Payload given to create_token function should only contain private claims and
+    should not hold any of the pre-registered interoperable claim names and values. */
+
 // Return a function with signOption object in its closure and assign function back to create_token
-/* Payload given to function should only contain private claims and should not hold any
-of the pre-registered interoperable claim names and values. */
-create_token = create_token(signOption);
+create_token = create_token(signOptions);
 // Return a function with verifyOption object in its closure and assign function back to verify
-verify = verify(verifyOption);
+verify_token = verify_token(verifyOptions);
 
 /*  Token verification middleware:
     To be passed in to the routes before the route handlers.
@@ -64,7 +66,7 @@ function v_mw(req, res, next) {
 // This is a middleware function that can be used on routes that require JWT security
 function get_token(req, res, next) {
     // Save token for subsequent functions to access token with request object after this middleware
-    req.token = extract_token(req);
+    req.token = extract_jwt_in_header(req);
     // End the req/res cycle if no token is sent
     if (typeof req.token === 'undefined')
         res.status(401).end(''); // If token does not exist or not sent over, respond with a 401 auth-token not provided
@@ -81,5 +83,8 @@ module.exports = {
 
     // The 2 modified versions for token signing and verification with the key in their closures
     create_token,
-    verify
+    verify_token,
+
+    // Expose public key through this module again
+    publicKey
 }
