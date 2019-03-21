05700a70a8f45b7f12e03b0d985b0e3a4e14930e
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-03-21 14:49:26 +0800

    Auth Service jwt module: 1.  Finnished building the Asymmetric key pair generation into the jwt module,     which allows me to directly add the private key into the function's closure     at program startup.
    2.  Cleaned up the code that does the partial application of the Asymmetric keys
    
    3.  Added 2 more functions, one of which extracts token from cookies and the
        other extracts the CSRF protection token from the Request header.

--- server/Auth_service/jwt.js
+++ server/Auth_service/jwt.js
@@ -2,18 +2,16 @@
 
 /*	@Doc Module description:
 	This module is a wrapper over the 'jsonwebtoken' package to create/read/sign/verify a token
-	This does not handle and authentication. Just token based Identity verification
 
     @TODO
+    - Create 1 Function to extract JWTs from header or token automatically.
     - Add a function to coerce Auth header to either all lower or upper case (No need if using Express)
     - Write unit test for this module
 	- look into private Keys and stuff like Asymmetric signing and verifying
 	- Create child processes too, to deal with the parsing and signing as it seems like it
       will take quite abit of CPU power
     - Start implementing JWEs
-
-    So at the start of this module, the PublicPrivate key pair will be generated and
-    automatically created...
+    - Perhaps the Public/Private key pair should be generated at a interval to change it constantly.
 */
 
 // Dependencies
@@ -24,42 +22,70 @@ const { promisify } = require('util');
 const sign = promisify(jwt.sign);
 const verify = promisify(jwt.verify);
 
-// Function to generate the Public-Private key pairs, either on startup on every setInterval?
-function key_setup() {
-    /* 
-        How does things like iBanking token from banks work. Can I implement that here?
-        So the services do not need to request for the public key from the auth service
-
-        Using the built in crypto modules? Perhaps find a way to generate the values
-        or if not try to find a library that can generate it for me
-    */
-}
 
 // Curried function of the original Promisified sign method
-const create_token = (private_key) => (signOption) => (payload) => sign(payload, private_key, signOption);
+var create_token = (private_key) => (signOption) => (payload) => sign(payload, private_key, signOption);
 
 // Curried function of the original Promisified verify method
-const verify_token = (public_key) => (verifyOption) => (token) => verify(token, public_key, verifyOption);
+var verify_token = (public_key) => (verifyOption) => (token) => verify(token, public_key, verifyOption);
 
 
+// Function to generate the Public/Private key pairs.
+function generateKeys() {
+    // Import and cache in memory only when used to generate keys. Garbage collected when function ends
+    const { generateKeyPairSync } = require('crypto');
 
-/*  Promisified version of jwt.sign method with the private key for Signing in its closure.
-    Resolves with the signed JWT, else
-    Rejects with an error.  */
+    // Generate a key with the RS256 algorithm.
+    return generateKeyPairSync('rsa', {
+        modulusLength: 1024, // Can be changed to be longer like 4096 for added security
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'pem'
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'pem',
+        }
+    });
+}
 
 
-/*  Promisified version of jwt.verify method with the public key for verification in its closure.
-    If signature is valid and the optional expiration, audience, or issuer are valid if given
-    Resolves with the decoded token, else
-    Rejects with an error.  */
+// Function to apply keys to curried functions for partial applications with keys in their closures
+function apply_keys() {
+    const { publicKey, privateKey } = generateKeys();
 
+    /*  Partial application with the private key for Signing in its closure.
+        Resolves with the signed JWT, else
+        Rejects with an error.  */
+    create_token = create_token(privateKey);
+
+    /*  Partial application with the public key for verification in its closure.
+        If signature is valid and the optional expiration, audience, or issuer are valid if given
+        Resolves with the decoded token, else
+        Rejects with an error.  */
+    verify_token = verify_token(publicKey);
+
+    // Return the public key for other services to use for verification, but let privateKey
+    // be destroyed when this function ends, to prevent it from being shared or anything
+    return publicKey;
+}
+
+// Apply keys into closures of the sign and verify functions and get back publicKey for verification
+const publicKey = apply_keys();
 
 
 /*  Pure function to extract token from request header and returns it
     FORMAT OF TOKEN --> Authorization: Bearer <access_token>
     Split on space, get token from array and return it.
     Express automatically coerces keys in the header object to be lowercase. */
-const extract_token = (req) => req.headers['authorization'].split(' ')[1];
+// Only for services where the JWT is passed in the Auth HTTP header
+const extract_jwt_in_header = (req) => req.headers['authorization'].split(' ')[1];
+
+// Only for web-apps where the JWT is passed as a cookie
+const extract_jwt_in_cookie = (req) => req.cookies.jwt;
+// Function for extracting CSRF token from the request from a web-app client
+const extract_CSRF_token = (req) => req.headers['x-csrf-token'];
+
 
 module.exports = {
     // The 2 promisified methods, Promisified with the Promisfy Util.
@@ -67,11 +93,14 @@ module.exports = {
     verify,
 
     // Token extraction function
-    extract_token,
+    // extract_jwt,
 
     // The 2 curried versions for token signing and verification with the key in their closures
     create_token,
-    verify_token
+    verify_token,
+
+    // The publicKey is also exported for other modules/services to verify the JWT
+    publicKey
 }
 
 
