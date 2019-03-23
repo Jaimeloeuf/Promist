74c27ee4438f6f7779b519d05d5dfc67ee26cd76
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-03-23 20:11:51 +0800

    Auth Service, token route module update: 1.  Updated the routing method to use Express.router object instead of the     imported app instance, following change in previous commit.
    2.  Cleaned up all the documentation in this module
    
    3.  Updated some code inside attach_token middleware following some API change.

--- server/Auth_service/routes/tokens.js
+++ server/Auth_service/routes/tokens.js
@@ -1,7 +1,9 @@
 'use strict'; // Enforce use of strict verion of JavaScript
 
 /*	@Doc
-    API routes for dealing with tokens
+    This module contains:
+    - Route handlers to deal with tokens
+    - 2 middleware functions, for authenticating the Creds and creating + attaching token
     
     1.  User posts credential to server on the '/login' route
     2.  Login route's handler will authenticate user's credential against those in the Database
@@ -17,7 +19,10 @@
     When user is not on the login page but wants to post creds. for JWTs, then the /token route should be used instead
 */
 
+const express = require('express');
+const router = express.Router();
 const auth = require('../auth');
+const { create_token, verify_token } = require('../token');
 
 // Middleware function for authenticating Credentials against Database credentials for user Object
 async function authenticate(req, res, next) {
@@ -46,11 +51,7 @@ async function authenticate(req, res, next) {
 // Middleware function for creating JWT payload for the client, creating and signing the JWT and finally attaching it for the user to use
 function attach_token(req, res, next) {
     // Base on the results by the authenticate middleware create a token for the user
-    const token = create_token({
-
-    })({
-
-    });
+    const token = create_token(req.user);
 
     // Attach token to res object differently based on request client type.
     //  If browser client, set token into cookie. Else if service or native app, put in auth header
@@ -59,33 +60,31 @@ function attach_token(req, res, next) {
 
     }
     else { // If service or native apps
-        res.header['Authorization'] = req.token
+        res.header['Authorization'] = token;
     }
-    /*  See how to revoke tokens or revoke cookies that is sent by the browser
-    */
+    //  See how to revoke tokens or revoke cookies that is sent by the browser
 }
 
-/*  Routes to get token from the service
-    2 Additional middlewares, one for authenticating the Creds, and one for creating and attaching token
-    
-    @Code-flow
+/*  @Code-flow
     Use the bodyParser middleware to read the user credentials from the request body
     Maybe Clean/Sanitize the credentials before using it against the DB?
     Get the password hash of user with "userID" from the DataBase
     compare it with the password user posted
     Create a JWT and send it back to the user
 */
-app.post('/login', express.json(), authenticate, attach_token);
-app.post('/tokens', express.json(), authenticate, attach_token);
-// See if the below format is possible
-app.post('/tokens or login', express.json(), authenticate, attach_token);
+router.post('/login', express.json(), authenticate, attach_token);
+router.post('/tokens', express.json(), authenticate, attach_token);
+// See if the below format is possible to combine the above 2 routes
+router.post('/tokens or login', express.json(), authenticate, attach_token);
 
 
 // Route to get a new JWT with a complimentary refresh token
-app.post('/tokens/refresh', express.json(), (req, res) => {
+router.post('/tokens/refresh', express.json(), (req, res) => {
     // Use the bodyParser middleware to read the refresh token in the request body
 
     // Verify that the refresh token is valid
 
     // Create a new token and return to the client
 });
+
+module.exports = router;
