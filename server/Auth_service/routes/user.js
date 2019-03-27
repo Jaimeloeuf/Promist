96b7092a7df5478882d5399ee127168df7d6f0b0
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-03-27 11:10:21 +0800

    Auth Service, user route module update: 1.  Updated the docs to include description of the service offered by API     endpoints defined in this module.
    2.  Added a new endpoint and route handler for the route to get user Object
        from the server.

--- server/Auth_service/routes/user.js
+++ server/Auth_service/routes/user.js
@@ -4,6 +4,13 @@
     API routes for users
     Routes in this module will provide expose the user CRUD operations via RESTful APIs
     All URL endpoints will have a '/user' prefixed to the routes
+
+    Services provided to clients:
+    - GET/Read entire user object
+    - DEL/Deactivate user from the system by adding a deactivated tag to the user object
+
+    Can I store a user object in a SQL DB? Is it I read already, then I recreate it into JS Obj
+    using something like a ORM? Or Scheme?
     
     @Todo
     - Modify the user route to be JWT protected
@@ -14,6 +21,16 @@
 
 const express = require('express');
 const router = express.Router();
+const db = require('../db/db');
+
+// (READ) Route to get the user object back from the DB
+router.get('/:userID', (req, res) => {
+    const { userID } = req.params;
+
+    db.get_user(userID)
+        .then(res.json)
+        .catch(error => res.status(error.code).send(error.message)); // Expects error thrown to be an object
+});
 
 // (READ) Route to get the user object back from the DB
 router.get('/:userID', (req, res) => {
