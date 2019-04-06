d040bc4207824cc838431cd0f2bb2c03f4377a71
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-04-06 18:38:13 +0800

    Auth Service, token module update: 1.  Cleaned up the module's documentation.
    2.  Removed some module todos.
    
    3.  Cleaned up all the documentation use comments throughout the module.
    
    4.  Created the new verify_token function and all have been tested to work.
    
    5.  Removed the call to the next middleware in the get_token middleware.

--- server/Auth_service/token.js
+++ server/Auth_service/token.js
@@ -2,27 +2,26 @@
 
 /*	@Doc Module description:
     - This module wraps over the jwt module to apply sign and verify options into the methods
-    from the jwt module before exporting the functions returned by these partial applications.
-    - Essentially the sign and verify options specific to this service is defined in here.
+      the jwt module before exporting the functions returned by these partial applications.
+    - User to specify the sign and verify options directly in this module for every service
+    - The DEFAULT OPTIONS MUST BE DEFINED, and they must be defined inside this module
+    - Essentially the sign and verify options specific to this service is defined here
+        - Their input interface is
+            (payload, ?options)   where options object is optional.
     - Payload given to create_token function should only contain private claims and
-    should not hold any of the pre-registered interoperable claim names and values.
+      should not hold any of the pre-registered interoperable claim names and values.
+
 
     @TODO
     - Finnish writing the verification middleware
-	- look into private Keys and stuff like Asymmetric signing and verifying
-	- Create child processes too, to deal with the parsing and signing as it seems like it
-      will take quite abit of CPU power
     - Start implementing JWEs (Is JWEs needed if there are stored in a HttpOnly cookie?)
 */
 
 // Dependencies
 const jwt = require('./jwt');
 
-/*  User to specify the sign and verify options directly in this module for every service
-    The OPTIONS MUST BE DEFINED, and they must be defined inside this module,
-    thus for the jwt module, the signOptions is the first function's parameter */
 
-// Token Signing OPTIONS
+// Default JWT Signing options object
 var signOptions = {
     issuer: 'Mysoft corp',
     subject: 'some@user.com',
@@ -32,7 +31,7 @@ var signOptions = {
     algorithm: 'RS256' // Must be RS256 as using asymmetric signing
 };
 
-// Token verification  OPTIONS
+// Default JWT Verification options object
 var verifyOptions = {
     issuer: 'Mysoft corp',
     subject: 'some@user.com',
@@ -41,47 +40,25 @@ var verifyOptions = {
     algorithm: ['RS256'] // Unlike signOption that we used while signing new token , we will use verifyOptions to verify the shared token by client. The only difference is, here the algorithm is Array [“RS256”].
 };
 
-/*  For above default sign and verify options, sometimes the user might want to
-    override them with different values for the properties.
 
-    Interface is changed to 
-    Instead of mutating the sign and verify options, create new objects
-*/
 // Utility function for merging. Returns an object made by merging the 2 input objects
+// To Create another merge function for deep merge using lodash's merge module
 const merge = (o1) => (o2) => ({ ...o1, ...o2 });
 
-// Create another merge function for deep merge using lodash's merge module
-
-// Convert the options into partially applied functions with the original object in the closure
-// Specialized function created by partial application
+/*  Partially apply options with merge function to get specialized functions with given options in
+    the closure, to apply to another options object with properties the user wants to override.
+    Bind these specialized functions to the original given options object's name */
 signOptions = merge(signOptions);
 verifyOptions = merge(verifyOptions);
 
-/*  ^   Self invoking anonymous function for applying options into the curried functions
-    Return a function with signOption object in its closure and assign function back to create_token
-    Return a function with verifyOption object in its closure and assign function back to verify */
-
-
-/*  The interface is
-    (payload, ?options)   where options is optional */
-
-
-// In the jsonwebtoken package, the options was entered last as an optional input. Maybe try this?
-// The optional options input will be used to override the options object defined in the tokens module
-// This ability to change the signOptions should only be available to the services that are signing
-// the different tokens, as there would be different needs for the different tokens, however for
-// verification, the options should remain the same.
-
 
 /*  Create new create and verify token functions in this namespace to export
-
-    Always merge with options object, because default value for it is alr an empty object
-    Merge with default options object to override properties if any is passed into the function
-    by finnishing application with the partial application function
-
-    The awaiting should be done by the function caller.
-*/
+    
+    - Always merge with default options object to override properties if any are passed
+      into the function by finnishing application with the partial application function.
+    - The awaiting should be done by the function caller.   */
 const create_token = (payload, options = {}) => jwt.create_token(signOptions(options))(payload);
+const verify_token = (payload, options = {}) => jwt.verify_token(verifyOptions(options))(payload);
 
 
 /*  Token verification middleware:
@@ -100,7 +77,7 @@ function v_mw(req, res, next) {
 }
 
 
-// This is a middleware function that can be used on routes that require JWT security
+// This is a Express middleware for routes that require JWT security
 function get_token(req, res, next) {
     // Save token for subsequent functions to access token with request object after this middleware
     req.token = jwt.extract_jwt_in_header(req);
@@ -108,9 +85,6 @@ function get_token(req, res, next) {
     if (typeof req.token === 'undefined')
         res.status(401).end(''); // If token does not exist or not sent over, respond with a 401 auth-token not provided
     // ^To update the response message, either with a 401 HTML page or smth
-
-    // Call the next middleware in the chain
-    next();
 }
 
 
